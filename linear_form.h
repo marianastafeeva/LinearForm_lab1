57b510c7746977b3d742b648f546657543ed3700
Marianna <maranastafeeva@gmail.com>
2026-05-06 20:17:11 +0300

    linear_form.h

--- linear_form.h
+++ linear_form.h
@@ -4,19 +4,20 @@
 #include "field_info.h"
 
 typedef struct {
-    void* coeffs; // массив значений подряд
-    int size; // количество коэффициентов
-    FieldInfo* field; // тип
+    void* coeffs;
+    int size;
+    FieldInfo* field;
 } LinearForm;
 
-LinearForm* CreateLinearForm(int size, FieldInfo* field);
-void SetCoefficient(LinearForm* lf, int index, void* value);
+LinearForm* lf_create(int size, FieldInfo* field_info);
+void lf_destroy(LinearForm* lf);
+void lf_set_coeff(LinearForm* lf, int index, const void* value);
+void* lf_get_coeff_ptr(const LinearForm* lf, int index);
 
-void AddLinearForm(LinearForm* a, LinearForm* b, LinearForm* result);
-void MultiplyByScalar(LinearForm* lf, void* scalar, LinearForm* result);
-
-void EvaluateLinearForm(LinearForm* lf, void** x, void* result);
-
-void FreeLinearForm(LinearForm* lf);
+void lf_add(const LinearForm* a, const LinearForm* b, LinearForm* result);
+void lf_sub(const LinearForm* a, const LinearForm* b, LinearForm* result);
+void lf_mul_scalar(const LinearForm* lf, const void* scalar, LinearForm* result);
+void lf_eval(const LinearForm* lf, const void** args, void* result);
 
 #endif
+
