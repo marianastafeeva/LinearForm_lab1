4b1406e811f824bff954d1e7f97b355cf13b903d
Marianna <maranastafeeva@gmail.com>
2026-05-06 20:20:06 +0300

    tests.h

--- tests.h
+++ tests.h
@@ -1,18 +1,6 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-void RunAllTests();
-
-void TestCreate();
-void TestSetCoefficient();
-void TestAdd();
-void TestMultiply();
-void TestEvaluate();
-
-void TestDifferentSizes();
-void TestOverwriteCoefficient();
-
-void TestDifferentTypes();
-void TestNullCoefficient();
+void RunAllTests(void);
 
 #endif
